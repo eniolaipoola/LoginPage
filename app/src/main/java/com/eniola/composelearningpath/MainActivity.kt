1b2b1a54f78204b5f1b406bd74fd0a55ac82e785
Eniola Ipoola <eniola.ipoola@identos.ca>
2022-04-29 20:41:38 +0100

    Initial project setup

---
+++ app/src/main/java/com/eniola/composelearningpath/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.eniola.composelearningpath
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
